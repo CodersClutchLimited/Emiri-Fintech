283d292fb23145cdc99557e49ef8eebc87004548
Ousainou BS Jammeh <ousainoubsj@gmail.com>
2025-01-03 10:47:16 +0000

    made the footer responsive across all screend

---
+++ public/javascripts/footer.js
@@ -0,0 +1,5 @@
+// document.getElementById('language-selector').addEventListener('change', function () {
+//     const selectedLang = this.value;
+//     const currentUrl = window.location.pathname;
+//     window.location.href = `${currentUrl}?lang=${selectedLang}`;
+// });
