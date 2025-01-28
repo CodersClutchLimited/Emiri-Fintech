618c400a220013ccb1f153665839f748f73fe4aa
Ousainou BS Jammeh <ousainoubsj@gmail.com>
2025-01-28 17:20:08 +0000

    worked on responsiveness for pages under blockchain

--- public/javascripts/nft.js
+++ public/javascripts/nft.js
@@ -97,22 +97,6 @@ document.querySelectorAll('.left-column a').forEach(link => {
     });
 });
 
-// table content smaller screen responsiveness handler
-document.querySelectorAll('.accordion-title-unique').forEach(title => {
-    title.addEventListener('click', () => {
-        const content = title.nextElementSibling;
-
-        // Toggle visibility of the content
-        content.classList.toggle('active');
-
-        // Optionally close other sections if required
-        document.querySelectorAll('.accordion-content-unique').forEach(section => {
-            if (section !== content) {
-                section.classList.remove('active');
-            }
-        });
-    });
-});
 
 document.addEventListener('DOMContentLoaded', function () {
     var swiper = new Swiper('.cosmic-swiper', {
