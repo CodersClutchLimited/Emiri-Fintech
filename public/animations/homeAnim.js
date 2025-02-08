ee0a717fe084d44b74d111ab62b1952331df3b76
Ousainou BS Jammeh <ousainoubsj@gmail.com>
2025-02-08 22:12:21 +0000

    Animated the home page

---
+++ public/animations/homeAnim.js
@@ -0,0 +1,200 @@
+ScrollReveal({ reset: true });
+ScrollReveal().reveal('.rob1', {
+    delay: 200,
+    distance: '50px',
+    origin: 'top',
+    easing: 'ease-in-out',
+    duration: 1000
+});
+
+ScrollReveal().reveal('.divider', {
+    delay: 400,
+    distance: '50px',
+    origin: 'bottom',
+    easing: 'ease-in-out',
+    duration: 1000
+});
+
+ScrollReveal().reveal('.para1', {
+    delay: 600,
+    distance: '50px',
+    origin: 'left',
+    easing: 'ease-in-out',
+    duration: 1000
+});
+
+ScrollReveal().reveal('.hero-bots', {
+    delay: 800,
+    distance: '50px',
+    origin: 'bottom',
+    easing: 'ease-in-out',
+    duration: 1000
+});
+
+ScrollReveal().reveal('.responsive-heading', {
+    delay: 200,
+    distance: '50px',
+    origin: 'left',
+    easing: 'ease-in-out',
+    duration: 1000
+});
+
+// Animate the subheading
+ScrollReveal().reveal('.responsive-subheading', {
+    delay: 400,
+    distance: '50px',
+    origin: 'left',
+    easing: 'ease-in-out',
+    duration: 1000
+});
+
+// Animate the grid boxes
+ScrollReveal().reveal('.software-2, .software-1', {
+    delay: 600,
+    distance: '50px',
+    origin: 'bottom',
+    easing: 'ease-in-out',
+    duration: 1000,
+    interval: 200 // Stagger the animations for each grid box
+});
+
+ScrollReveal().reveal('.column-1', {
+    delay: 200,
+    distance: '50px',
+    origin: 'left',
+    easing: 'ease-in-out',
+    duration: 1000
+});
+
+// Animate the second column
+ScrollReveal().reveal('.column-2', {
+    delay: 400,
+    distance: '50px',
+    origin: 'right',
+    easing: 'ease-in-out',
+    duration: 1000
+});
+
+// Animate the headings
+ScrollReveal().reveal('.column-1-title, .column-2-title', {
+    delay: 600,
+    distance: '30px',
+    origin: 'top',
+    easing: 'ease-in-out',
+    duration: 1000
+});
+
+// Animate the paragraphs
+ScrollReveal().reveal('.column-1-text, .column-2-text', {
+    delay: 800,
+    distance: '30px',
+    origin: 'bottom',
+    easing: 'ease-in-out',
+    duration: 1000
+});
+
+// Animate the images
+ScrollReveal().reveal('.column-1 img, .column-2 img', {
+    delay: 1000,
+    distance: '50px',
+    origin: 'bottom',
+    easing: 'ease-in-out',
+    duration: 1000
+});
+
+// Animate the buttons
+ScrollReveal().reveal('.column-1 .button, .column-2 .button', {
+    delay: 1200,
+    distance: '30px',
+    origin: 'bottom',
+    easing: 'ease-in-out',
+    duration: 1000
+});
+
+ScrollReveal().reveal('.four-1, .four-2', {
+    delay: 200,
+    distance: '30px',
+    origin: 'left',
+    easing: 'ease-in-out',
+    duration: 1000,
+    interval: 100 // Stagger the animations for each line
+});
+
+// Animate the list items
+ScrollReveal().reveal('.four-list p', {
+    delay: 400,
+    distance: '30px',
+    origin: 'left',
+    easing: 'ease-in-out',
+    duration: 1000,
+    interval: 100 // Stagger the animations for each list item
+});
+
+// Animate the right column image
+ScrollReveal().reveal('.four-right-column img', {
+    delay: 600,
+    distance: '50px',
+    origin: 'right',
+    easing: 'ease-in-out',
+    duration: 1000
+});
+
+ScrollReveal().reveal('.find-taurus button', {
+    delay: 200, // Delay before the animation starts
+    distance: '50px', // Distance the button moves
+    origin: 'bottom', // Direction from which the button animates
+    easing: 'ease-in-out', // Smooth easing
+    duration: 1000 // Duration of the animation
+});
+
+ScrollReveal().reveal('.text-starter', {
+    delay: 200,
+    distance: '50px',
+    origin: 'left',
+    easing: 'ease-in-out',
+    duration: 1000
+});
+
+// Animate the first column text
+ScrollReveal().reveal('.grid-1', {
+    delay: 400,
+    distance: '50px',
+    origin: 'bottom',
+    easing: 'ease-in-out',
+    duration: 1000
+});
+
+// Animate the second column text
+ScrollReveal().reveal('.grid-2', {
+    delay: 600,
+    distance: '50px',
+    origin: 'bottom',
+    easing: 'ease-in-out',
+    duration: 1000
+});
+
+ScrollReveal().reveal('.logo-item', {
+    delay: 200,
+    distance: '50px',
+    origin: 'bottom',
+    easing: 'ease-in-out',
+    duration: 1000,
+    interval: 200 // Stagger animations for each logo item
+});
+
+// Optional: Add special animations for specific logos
+ScrollReveal().reveal('.special-logo', {
+    delay: 400,
+    distance: '50px',
+    origin: 'left',
+    easing: 'ease-in-out',
+    duration: 1000
+});
+
+ScrollReveal().reveal('.special-logo-2', {
+    delay: 600,
+    distance: '50px',
+    origin: 'right',
+    easing: 'ease-in-out',
+    duration: 1000
+});
